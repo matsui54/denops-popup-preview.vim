7412d8cbb1252c52d5550dcd2fbc3cd0233a2f4c
matsui54 <haru.matu9168@gmail.com>
2023-01-07 16:51:02 +0900

    Remove github workflow

--- denops/popup_preview/deps_test.ts
+++ denops/popup_preview/deps_test.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/std@0.168.0/testing/asserts.ts";
-export * from "https://deno.land/x/denops_std@v3.12.0/test/mod.ts";
+export * from "https://deno.land/std@0.171.0/testing/asserts.ts";
+export * from "https://deno.land/x/denops_std@v3.12.1/test/mod.ts";
