54b76e9717ce994d7c9d0ec3c9494a941dc9ea41
matsui54 <haru.matu9168@gmail.com>
2025-08-11 18:17:47 +0900

    :package: bump @denops/std from 7.5.0 to 7.6.0

--- denops/popup_preview/deps.ts
+++ denops/popup_preview/deps.ts
@@ -1,14 +1,11 @@
-export type { Denops } from "jsr:@denops/std@7.5.0";
-export {
-  batch,
-  collect,
-} from "jsr:@denops/std@7.5.0/batch";
-export * as op from "jsr:@denops/std@7.5.0/option";
+export type { Denops } from "jsr:@denops/std@7.6.0";
+export { batch, collect } from "jsr:@denops/std@7.6.0/batch";
+export * as op from "jsr:@denops/std@7.6.0/option";
 export * as path from "jsr:@std/path@1.1.0";
-export * as fn from "jsr:@denops/std@7.5.0/function";
-export * as nvimFn from "jsr:@denops/std@7.5.0/function/nvim";
-export * as vars from "jsr:@denops/std@7.5.0/variable";
-export * as autocmd from "jsr:@denops/std@7.5.0/autocmd";
+export * as fn from "jsr:@denops/std@7.6.0/function";
+export * as nvimFn from "jsr:@denops/std@7.6.0/function/nvim";
+export * as vars from "jsr:@denops/std@7.6.0/variable";
+export * as autocmd from "jsr:@denops/std@7.6.0/autocmd";
 export {
   is,
   type Predicate,
